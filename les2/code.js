b6782b564f2ecf82a3e6a7b14b39e020f492a503
Nikolai <newsake@yandex.ru>
2019-03-03 20:43:59 +0300

    Правка

---
+++ les2/code.js
@@ -0,0 +1,40 @@
+let money = +prompt("Укажите вашь бюджет на месясц: ");
+let time = prompt("Введите дату в формате YYYY-MM-DD: ", "2019-00-01");
+
+let appData = {
+    money: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses:  {},
+    income: [],
+    savings: false
+};
+
+// q - сокращенное от слова question ( вопрос ).
+for (let i = 0; i < 2; i++) {
+    let q1 = prompt("Введите обязательную статью расходов в этом месяце: ");
+    let q2 = +prompt("Во сколько обойдется? ");
+
+    if ( (typeof(q1) != null) && (typeof(q2) != null) &&
+        (typeof(q1) != "" && (typeof(q2) != "")) ) {
+        appData.expenses[q1] = q2;
+    } else {
+        
+    };
+    
+};
+
+appData.moneyForDay = appData.money / 30;
+
+if (appData.moneyForDay < 100) {
+    console.log ("Минимальный уровень достатка");
+} else if (appData.moneyForDay > 100 && appData.moneyForDay < 1000) {
+    console.log ("Средний уровень достатка");
+} else if (appData.moneyForDay > 2000) {
+    console.log ("Высокий уровень достатка");
+} else {
+    console.log ("Ошибка !");
+}
+
+console.log(appData.moneyForDay);
+
---
+++ les2/les2.2/script.js
@@ -0,0 +1,11 @@
+
+let weekDay = [];
+for (let i = 0; i < 7; i++) {
+    let day = prompt ("Видети день недели по очереди начиная с Понедельника:");
+    weekDay.push(day);
+}
+
+let h = document.createElement("h1");
+
+
+console.log(weekDay);
