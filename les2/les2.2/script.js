d41aeefd9b06ffa9b1965ed225ae9cbd643a2146
Nikolai <newsake@yandex.ru>
2019-04-17 21:12:00 +0300

    Закрыл

--- les2/les2.2/script.js
+++ les2/les2.2/script.js
@@ -3,9 +3,15 @@ let weekDay = [];
 for (let i = 0; i < 7; i++) {
     let day = prompt ("Видети день недели по очереди начиная с Понедельника:");
     weekDay.push(day);
+    document.write (day + "</br>");
 }
 
+function bould () {
+    bolud1 = weekDay[6];
+    bolud1 = document.body.style.background: 'green';
+}
 
+bould ();
 
 
 console.log(weekDay);
